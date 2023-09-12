fa45ac09204b25aada645b9574cc787cbf4a1063
AlejandroPacheco <alejandro.pacheco.ferro@gmail.com>
2023-09-12 21:05:39 +0200

    Fixed duplicated loginUser

--- backend/controllers/users/loginUser.js
+++ backend/controllers/users/loginUser.js
@@ -31,68 +31,6 @@ async function loginUser (req, res, next) {
             return next(generateError('Email y/o contraseña incorrectos', 404));
         }
 
-        if (userInfo[0].active === 0) {
-            return next(generateError('Debes activar tu usuario primero. Revisa la bandeja de entrada o spam de tu correo.', 403));
-        }
-
-        const payload = {
-            id: userInfo[0].id,
-            role: userInfo[0].role
-        };
-
-        const { SECRET } = process.env;
-
-        const token = jwt.sign(payload, SECRET, { expiresIn: '1w' });
-
-        res.header({ token });
-
-        res.status(200).send({
-            status: 'Ok',
-            message: 'Usuario logueado correctamente',
-            data: {
-                userInfo,
-                token
-            }
-        });
-    } catch (error) {
-        next(error);
-    }
-}
-
-module.exports = loginUser;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const getConnection = require('../../db/connectDB');
-const loginUserSchema = require('../../schema/loginUserSchema');
-const generateError = require('../../helpers/generateError');
-
-async function loginUser (req, res, next) {
-    const { error } = loginUserSchema.validate(req.body);
-
-    if (error) {
-        return next(generateError(error.details[0].message, 400));
-    }
-
-    try {
-        const pool = await getConnection();
-
-        const { email, password } = req.body;
-
-        const [userInfo] = await pool.query(
-            'SELECT * FROM users WHERE email = ?;'
-            , [email]);
-
-        if (userInfo.length < 1) {
-            return next(generateError('Email y/o contraseña incorrectos', 404));
-        }
-
-        const validatePassword = await bcrypt.compare(password, userInfo[0].password);
-
-        if (!validatePassword) {
-            return next(generateError('Email y/o contraseña incorrectos', 404));
-        }
-
         if (userInfo[0].active === false) {
             return next(generateError('Debes activar tu usuario primero. Revisa la bandeja de entrada o spam de tu correo.', 403));
         }
