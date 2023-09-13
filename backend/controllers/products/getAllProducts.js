fb131c8d3de4764c89f3eb91ca472264d148fcfe
Juli <elkeloko@gmail.com>
2023-09-13 11:14:25 +0100

    improvement getAllProducts with user and user reviews

--- backend/controllers/products/getAllProducts.js
+++ backend/controllers/products/getAllProducts.js
@@ -3,8 +3,25 @@ const getPool = require('../../db/connectDB');
 async function getAllProducts (req, res, next) {
     try {
         const pool = await getPool();
-        const [products] = await pool.query(`SELECT id, name, description, price, category, user_id 
-                                          FROM products `);
+        const [products] = await pool.query(`SELECT
+        p.id AS product_id,
+        p.name AS product_name,
+        p.description AS product_description,
+        p.price AS product_price,
+        p.category AS product_category,
+        p.user_id AS seller_id,
+        u.first_name AS seller_first_name,
+        u.last_name AS seller_last_name,
+        r.title AS review_title,
+        r.text AS review_text,
+        r.stars AS review_stars
+    FROM
+        products AS p
+    JOIN
+        users AS u ON p.user_id = u.id
+    LEFT JOIN
+        reviews AS r ON p.id = r.product_id;
+    `);
         res.status(200).send({
             status: 'Ok',
             message: 'Productos disponibles',
