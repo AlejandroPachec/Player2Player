a5c0165360db776efde6e70823525c4da321f382
AlejandroPacheco <alejandro.pacheco.ferro@gmail.com>
2023-09-14 21:59:37 +0200

    Fixed bug addProduct

--- backend/controllers/products/addProduct.js
+++ backend/controllers/products/addProduct.js
@@ -3,16 +3,27 @@ const getPool = require('../../db/connectDB');
 const addProductSchema = require('../../schema/addProductSchema');
 const generateError = require('../../helpers/generateError');
 const savePhoto = require('../../helpers/savePhoto');
+const { photoSchema, arrayPhotoSchema } = require('../../schema/PhotoSchema');
+
 
 async function addProduct (req, res, next) {
-    const { error } = addProductSchema.validate(req.body);
-    if (error) {
-        return next(generateError(error.details[0].message, 400));
-    }
+    let insertedPhotos = [];	
+    let errorSchema; 
+
+    if (Array.isArray(req.files.photos)) {
+        const { error } = arrayPhotoSchema.validate(req.files.photos)
+        errorSchema = error;
+    } else {
+        const { error } = photoSchema.validate(req.files.photos);
+        errorSchema = error;
+    } 
+
+    if (errorSchema) {
+        return next(generateError(errorSchema.details[0].message, 400));
+    };
 
     try {
         const pool = await getPool();
-
         const { name, description, price, category, state } = req.body;
 
         // //* Verificamos que no exista un producto con la misma descripción
@@ -26,7 +37,6 @@ async function addProduct (req, res, next) {
 
         const id = crypto.randomUUID();
 
-        
         //* Damos error si no subimos ninguna foto
         
         if (!req.files?.photos) {
@@ -35,31 +45,27 @@ async function addProduct (req, res, next) {
         
         //* Guardamos e insertamos las fotos
         
-        let insertedPhotos = [];	
-        if (Object.keys(req.files).length >= 1 && Object.values(req.files.photos).length < 8) {
-            console.log("cheguei aqui");
-            for (const photo of Object.values(req.files.photos)) {
-                const photoName = await savePhoto(photo, 500);
-                
-                const photo_id = crypto.randomUUID();
-                
-                await pool.query(
-                    `INSERT INTO product_photo (id ,name, product_id) VALUES (?, ?, ?)`,
-                    [photo_id, photoName, id]
-                    );
-                    
-                    insertedPhotos.push(photoName);
-                    
-                };
-                
-                await pool.query('INSERT INTO products(id, name, description, price, category, state, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)', [id, name, description, price, category, state, req.user.id]);
-                
-            } else if (Object.keys(req.files.photos).length > 7) {
-                return next(generateError('Has subido más de las 7 fotos permitidas', 400));
-            };
-            
-            //* Insertamos el producto
+        if (req.files.photos.length > 8) {
+            return next(generateError('Has subido demasiadas fotos. Máximo 8', 400));
+        };
         
+        await pool.query('INSERT INTO products(id, name, description, price, category, state, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)', [id, name, description, price, category, state, req.user.id]);
+        
+        for (const photo of Object.values(req.files.photos)) {
+            const photoName = await savePhoto(photo, 500);
+            
+            const photo_id = crypto.randomUUID();
+            
+            await pool.query(
+                `INSERT INTO product_photo (id ,name, product_id) VALUES (?, ?, ?)`,
+                [photo_id, photoName, id]
+                );
+                
+                insertedPhotos.push(photoName);
+                
+            };
+        
+            //* Insertamos el producto
         res.status(200).send({
             status: 'Ok',
             message: 'Producto creado correctamente',
---
+++ backend/schema/PhotoSchema.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+
+const photoSchema = Joi.object({
+  name: Joi.string().required(),
+  data: Joi.any().required(),
+  size: Joi.number().required(),
+  encoding: Joi.string(),
+  tempFilePath: Joi.any(),
+  truncated: Joi.boolean(),
+  mimetype: Joi.string().valid('image/jpg', 'image/jpeg', 'image/png').required(),
+  md5: Joi.string(),
+  mv: Joi.func(),
+});
+
+const arrayPhotoSchema = Joi.array().items(photoSchema);
+
+module.exports = { photoSchema, arrayPhotoSchema }
