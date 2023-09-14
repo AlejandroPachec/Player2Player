bc7745be7dd92a662fb27056c3e647698158065f
AlejandroPacheco <alejandro.pacheco.ferro@gmail.com>
2023-09-14 17:58:21 +0200

    Fixed product related files

--- backend/schema/addProductSchema.js
+++ backend/schema/addProductSchema.js
@@ -4,9 +4,13 @@ const addProductSchema = Joi.object({
     name: Joi.string().min(2).max(50).required(),
     description: Joi.string().min(2).max(500).required(),
     price: Joi.number().positive().max(100000).required(),
-    product_image: Joi.string().max(100),
     category: Joi.any().valid('Consolas', 'Videojuegos', 'Accesorios', 'Retro', 'Ordenadores').required(),
-    state: Joi.any().valid('Nuevo', 'En buen estado', 'Aceptable', 'No da para más').required()
+    state: Joi.any().valid('Nuevo', 'En buen estado', 'Aceptable', 'No da para más').required(),
+    // photos: Joi.object({
+    //     fieldName: Joi.string().valid('file'),
+    //     originalNme: Joi.string(),
+    //     mimetype: Joi.string().valid('image/jpeg', 'image/png', 'image/jpg')
+    // })
 });
 
 module.exports = addProductSchema;
