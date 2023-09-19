8d43d28d13f1218f75d8ede5b365f3ca4fe241b5
Nanna <nanaa.gonzalez@gmail.com>
2023-09-19 21:19:21 +0200

    Add CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const cors = require('cors');
 const app = express();
-const { PORT } = require('./config'); 
+const { PORT } = require('./config');
 
 const userRouter = require('./routes/userRouter');
 const productsRouter = require('./routes/productRouter');
 const ordersRouter = require('./routes/ordersRouter');
 const reviewsRouter = require('./routes/reviewsRouter');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(express.json({ limit: '10mb' }));
-
-app.use(express.json());
 app.use(fileUpload());
 
 app.use('/user', userRouter);
