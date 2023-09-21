39a6cd7d0d9f94abd47280e4e78ce632f8b2bc05
Nanna <nanaa.gonzalez@gmail.com>
2023-09-21 10:46:30 +0200

    Fix routes for email verification

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,14 +3,14 @@ const userRouter = express.Router();
 const authUser = require('../middlewares/authUser');
 
 const createUser = require('../controllers/users/createUser');
-/* const activateUser = require('../controllers/users/activateUser'); */
+const activateUser = require('../controllers/users/activateUser');
 const loginUser = require('../controllers/users/loginUser');
 const getUser = require('../controllers/users/getUser');
 const editUser = require('../controllers/users/editUser');
 
 
 userRouter.post('/create', createUser);
-/* userRouter.get('/activate/:registrationCode', activateUser); */
+userRouter.get('/activate/:registrationCode', activateUser);
 userRouter.post('/login', loginUser);
 
 userRouter.get('/profile/:userId', getUser);
