488423825a8f41dae34a9c1c3d8cb06a50a0d09d
Alejandro Pacheco Ferro <138246057+AlejandroPachec@users.noreply.github.com>
2023-09-21 09:40:01 +0200

    Updated config

--- backend/config.js
+++ backend/config.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5002;
-const MYSQL_USER = process.env.MYSQL_USER || 'root';
-const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '1234';
-const MYSQL_PORT = process.env.MYSQL_PORT || '3306';
-const MYSQL_DB_NAME = process.env.MYSQL_DB_NAME || 'p2p_db';
-const SMTP_USER = process.env.SMTP_USER || 'player2playershop@gmail.com';
-const SMTP_PASSWORD = process.env.SMTP_PASSWORD || 'xsmtpsib-f743ea96fa21218569f4f9106f466726e3dbc5c76a59129ef009174a300874e9-46gRYjJOxt7E39kH';
-const SECRET = process.env.SECRET || 'maoihd093hdaknefifa3fadc';
-const UPLOADS_DIR = process.env.UPLOADS_DIR || 'uploads';
+const PORT = process.env.PORT;
+const MYSQL_USER = process.env.MYSQL_USER;
+const MYSQL_HOST = process.env.MYSQL_HOST;
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
+const MYSQL_PORT = process.env.MYSQL_PORT;
+const MYSQL_DB_NAME = process.env.MYSQL_DB_NAME;
+const SMTP_USER = process.env.SMTP_USER;
+const SMTP_PASSWORD = process.env.SMTP_PASSWORD;
+const SECRET = process.env.SECRET;
+const UPLOADS_DIR = process.env.UPLOADS_DIR;
 
 module.exports = {
     PORT,
