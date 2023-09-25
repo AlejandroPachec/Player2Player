1ba4b19918434c658047b8afa22514c79b102403
Nanna <nanaa.gonzalez@gmail.com>
2023-09-25 12:27:34 +0200

    Add first general components

---
+++ frontend/src/components/loggedInAvatar/LoggedInAvatar.jsx
@@ -0,0 +1,12 @@
+import './loggedInAvatar.css';
+
+const LoggedInAvatar = () => {
+    return (
+        <div className='loggedInAvatar'>
+            <p>Samuel</p>
+            <img src="" alt="Avatar de usuario" className='avatarLoggedIn'/>
+        </div>
+    );
+};
+
+export default LoggedInAvatar;
