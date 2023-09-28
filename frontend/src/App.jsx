3f85cbbbb65f0aed2f53ac35e946990dea0c59cf
Nanna <nanaa.gonzalez@gmail.com>
2023-09-28 10:56:59 +0200

    Add registerPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home-page/HomePage';
 import LoginPage from './pages/login-page/LoginPage';
 import SearchPage from './pages/search-page/SeacrhPage';
+import RegisterPage from './pages/register-page/RegisterPage';
 
 function App () {
     return (
@@ -9,8 +10,8 @@ function App () {
             <Route path='/' element={<HomePage/>} />
             <Route path='/:params' element={<SearchPage/>}/>
             <Route path='/user/login' element={<LoginPage/>} />
-            {/* <Route path='/user/create' element={RegisterPage} />
-            <Route path='*' element={NotFound} /> */}
+            <Route path='/user/create' element={<RegisterPage/>} />
+            {/* <Route path='*' element={NotFound} /> */}
         </Routes>
     );
 }
--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -21,3 +21,20 @@ export const getSearchProductsService = async (params) => {
 
     return data.data;
 };
+
+
+export const registerUserService = async ({ firstName, lastName, email, phone, password }) => {
+    const response = await fetch('http://localhost:5002/user/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ firstName, lastName, email, phone, password })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.error);
+    }
+};
