c2df13f2550a812df153ebf14bf5a1758a0420c0
Nanna <nanaa.gonzalez@gmail.com>
2023-10-01 16:10:40 +0200

    Modify the data received from the endpoint getAllUserOrders

--- backend/controllers/orders/getAllUserOrders.js
+++ backend/controllers/orders/getAllUserOrders.js
@@ -6,35 +6,39 @@ async function getAllUserOrder (req, res, next) {
         const { idUser } = req.params;
 
 
-        const [[ordersInfo]] = await pool.query(`
-        SELECT 
+        const [ordersInfo] = await pool.query(`
+        SELECT
+        O.id,
         O.product_id,
         O.created_at,
         O.user_buyer_id,
+        O.status,
         P.name AS product_name,
         P.description,
         P.price,
         U.first_name AS seller_first_name,
         U.last_name AS seller_last_name,
         U.avatar,
-        U.city
-        FROM 
-        orders O
-        INNER JOIN 
-        products P 
-        ON O.product_id = P.id
-        INNER JOIN
-        users U
-        ON O.user_buyer_id = U.id
-        WHERE 
-        O.user_seller_id = ?;
+        U.city,
+        MAX(PP.name) AS product_photo
+        FROM orders O
+        INNER JOIN products P ON O.product_id = P.id
+        INNER JOIN (SELECT product_id, MAX(name) AS name
+        FROM product_photo
+        GROUP BY product_id
+        ) AS PP ON O.product_id = PP.product_id
+        INNER JOIN users U ON O.user_buyer_id = U.id
+        WHERE
+        O.user_seller_id = ?
+        GROUP BY O.id, O.product_id, O.created_at, O.user_buyer_id, O.status, P.name, 
+        P.description, P.price, U.first_name, U.last_name, U.avatar, U.city;
         `, [idUser]);
 
         const [[rating]] = await pool.query(`
         SELECT AVG(stars) AS userAvgReviews FROM reviews
         WHERE user_buyer_id = ?
         GROUP BY user_buyer_id;        
-        `, [ordersInfo.user_buyer_id]);
+        `, [ordersInfo[0].user_buyer_id]);
 
         res.status(200).send({
             status: 'Ok',
