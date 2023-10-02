651daac9bdb1032208a2462aeb926465385e05aa
Alejandro Pacheco Ferro <138246057+AlejandroPachec@users.noreply.github.com>
2023-10-02 23:10:09 +0200

    Working on OrderManagementPage and ExchangeSetPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,12 @@ import AddProductPage from './pages/addProduct-page/AddProductPage';
 import OrderManagementPage from './pages/orderManagement-page/OrderManagementPage';
 import EditUserPage from './pages/editUser-page/EditUserPage';
 import SeeOrdersPage from './pages/see-orders-page/SeeOrdersPage';
-
+import ExchangeSetPage from './pages/exchangeSet-page/ExchangeSetPage';
 function App () {
     return (
         <Routes>
             <Route path='/product/addProduct' element={<AddProductPage/>} />
+            <Route path='/order/exchangeSet/:idOrder' element={<ExchangeSetPage/>} />
             <Route path='/user/edit' element={<EditUserPage/>} />
             <Route path='/user/orders' element={<SeeOrdersPage/>}/>
             <Route path='/user/orders/:sellerUser' element={<OrderManagementPage/>} />
