337abce266cf8626cc555fc0fa8aaa7761771078
Nanna <nanaa.gonzalez@gmail.com>
2023-10-04 15:06:59 +0200

    Add more information to getUserOrder response

--- backend/controllers/orders/getUserOrder.js
+++ backend/controllers/orders/getUserOrder.js
@@ -12,7 +12,11 @@ async function getUserOrder (req, res, next) {
 
         if (orderId) {
             query = `
-                SELECT O.*, P.name, P.description, P.price, P.state, P.created_at, PP.product_photo, UB.first_name, UB.last_name
+                SELECT O.*, P.name, P.description, P.price, P.category, 
+                P.state, P.created_at, PP.product_photo, UB.first_name, UB.last_name, 
+                UB.avatar, AVG(stars) AS userAvgReviews,
+                U.first_name AS seller_first_name, U.last_name AS seller_last_name,
+                U.avatar AS seller_avatar
                 FROM orders O
                 INNER JOIN products P ON O.product_id = P.id
                 LEFT JOIN LATERAL (
@@ -20,9 +24,15 @@ async function getUserOrder (req, res, next) {
                 FROM product_photo
                 WHERE product_id = O.product_id
                 ) AS PP ON true
-                INNER JOIN users UB ON UB.id = O.user_buyer_id 
-                WHERE O.id = ? AND O.user_seller_id = ?;
-            `;
+                INNER JOIN users UB ON UB.id = O.user_buyer_id
+                INNER JOIN users U ON U.id = O.user_seller_id
+                INNER JOIN reviews R ON O.user_buyer_id = R.user_buyer_id
+                WHERE O.id = ? AND O.user_buyer_id = ?
+                GROUP BY O.id, O.exchange_place, O.exchange_time,  P.name, P.description, P.price, P.category, P.state, P.created_at,
+                PP.product_photo,
+                UB.first_name, UB.last_name, UB.avatar;
+            `
+            ;
             queryParams = [orderId, userId];
         } else {
             query = `
