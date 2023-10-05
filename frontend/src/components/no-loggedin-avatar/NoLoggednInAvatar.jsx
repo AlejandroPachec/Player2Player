a60415bf7d98ab7ff64e2d2e0db3874c13f4fc52
Nanna <nanaa.gonzalez@gmail.com>
2023-10-05 10:22:48 +0200

    Add styles at notFoundPage

--- frontend/src/components/no-loggedin-avatar/NoLoggednInAvatar.jsx
+++ frontend/src/components/no-loggedin-avatar/NoLoggednInAvatar.jsx
@@ -1,9 +1,10 @@
 import signInAvatar from '../../assets/singIn.svg';
 import { Link } from 'react-router-dom';
+import './noLoggedIn.css';
 
 function NoLoggedInAvatar () {
     return (
-        <Link to='/user/login'><img src={signInAvatar} alt="sign in avatar"/></Link>
+        <Link to='/user/login'><img src={signInAvatar} alt="sign in avatar" className='default-avatar'/></Link>
     );
 }
 
--- frontend/src/pages/notFound-page/NotFoundPage.jsx
+++ frontend/src/pages/notFound-page/NotFoundPage.jsx
@@ -3,6 +3,8 @@ import Footer from '../../components/footer/Footer';
 import MainButton from '../../components/main-button/MainButton';
 import gameOverImage from '../../assets/space.webp';
 import { useNavigate } from 'react-router-dom';
+import './notfound.css';
+
 const NotFoundPage = () => {
     const navigate = useNavigate();
 
@@ -13,10 +15,10 @@ const NotFoundPage = () => {
     return (
         <>
             <HeaderSecond />
-            <main>
+            <main className='not-found'>
                 <img src={gameOverImage} alt="Imagen de Game Over" />
-                <h1>GAME OVER</h1>
-                <h2>PUNTUACIÓN 404</h2>
+                <h1>GAME <br /><span>OVER</span></h1>
+                <h2>PUNTUACIÓN <span>404</span></h2>
                 <p>¡Más suerte la próxima vez!</p>
                 <MainButton text={'Volver a cargar'} handleClick={handleClick}/>
             </main>
