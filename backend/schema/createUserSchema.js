116c42b26d70ef1e1d3e6b59f444bf156eef1b43
Nanna <nanaa.gonzalez@gmail.com>
2023-10-05 12:27:42 +0200

    Modifiy schemas

--- backend/schema/createUserSchema.js
+++ backend/schema/createUserSchema.js
@@ -9,7 +9,7 @@ const createUserSchema = Joi.object({
         'any.required': 'El nombre es obligatorio'
     }),
     lastName: Joi.string().min(2).max(50).pattern(/^[A-Za-zñÑ\sáéíóúÁÉÍÓÚüÜ]+$/u).required().messages({
-        'string.empty': 'El apllido no puede estar vacío',
+        'string.empty': 'El apellido no puede estar vacío',
         'string.min': 'El apellido tiene que tener mínimo 2 letras',
         'string.max': 'El apellido no puede tener más de 50 letras',
         'string.pattern.base': 'El apellio solo puede contener letras y espacios',
@@ -22,7 +22,7 @@ const createUserSchema = Joi.object({
         'any.required': 'El email es obligatorio'
     }),
     password: Joi.string().required().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-zñÑ\d@$!%*?&]{8,20}$/).messages({
-        'string.empty': 'El password no puede estar vacío',
+        'string.empty': 'La contraseña no puede estar vacía',
         'string.pattern.base': 'La contraseña debe tener entre 8 y 20 caracteres, contener una minúscula, una mayúscula, un número y un caracter especial.',
         'any.required': 'La contraseña es obligatoria'
     }),
