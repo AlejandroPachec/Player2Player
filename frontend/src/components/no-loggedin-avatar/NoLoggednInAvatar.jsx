ee47af3a2f729690a71d3dfefc05185308bb06a8
Nair González <108533676+Nannitta@users.noreply.github.com>
2023-10-05 10:41:55 +0200

    Revert "Add styles at notFoundPage"

--- frontend/src/components/no-loggedin-avatar/NoLoggednInAvatar.jsx
+++ frontend/src/components/no-loggedin-avatar/NoLoggednInAvatar.jsx
@@ -1,10 +1,9 @@
 import signInAvatar from '../../assets/singIn.svg';
 import { Link } from 'react-router-dom';
-import './noLoggedIn.css';
 
 function NoLoggedInAvatar () {
     return (
-        <Link to='/user/login'><img src={signInAvatar} alt="sign in avatar" className='default-avatar'/></Link>
+        <Link to='/user/login'><img src={signInAvatar} alt="sign in avatar"/></Link>
     );
 }
 
