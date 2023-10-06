af83d49fb3f944e034fc8269fdab9a01320ba5cb
Alejandro Pacheco Ferro <138246057+AlejandroPachec@users.noreply.github.com>
2023-10-07 00:34:19 +0200

    Fix UserAuthContext

--- frontend/src/context/UserAuthContext.jsx
+++ frontend/src/context/UserAuthContext.jsx
@@ -30,8 +30,10 @@ const UserAuthContextProvider = ({ children }) => {
         setUser(prevUser => {
             const updatedUser = { ...prevUser };
             for (const [key, value] of Object.entries(newUser)) {
-                if (value !== '' && key === 'password' && key === 'phone' && key === 'email' && key === 'city' && key === 'bio') {
-                    updatedUser[key] = value;
+                if (value !== '') {
+                    if (key !== 'password' && key !== 'phone' && key !== 'email' && key !== 'city' && key !== 'bio') {
+                        updatedUser[key] = value;
+                    }
                 }
             }
             return updatedUser;
